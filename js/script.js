717cc69df5c5964b3385d8cbab80cf1680dc6713
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-06-29 11:13:48 +0200

    Click message

--- js/script.js
+++ js/script.js
@@ -52,9 +52,15 @@ const app = new Vue(
                     ],
                 }
             ]
-
-
-
+        },
+        methods: {
+            addMessage(index) {
+                console.log(this.contacts[index].messages);
+                const messagge = this.contacts[index].messages;
+                for (var i = 0; i < contacts.length; i++) {
+                    document.getElementById('message').innerHTML = `${this.contacts[index].messages}`
+                }
+            }
         }
     }
 );
